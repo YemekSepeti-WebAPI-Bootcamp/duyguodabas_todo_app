cb870a737ff0e6d6e204a272bf8737045768d967
duyguevrim <duyguevrimodabass@gmail.com>
2021-03-12 20:06:37 +0300

    Update TodoList.js
    css duzenlendi

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -19,65 +19,16 @@ const TodoList = () => {
                     </ul>
                 </div>
                 <style jsx>{`
-                  .text-muted {
-                    color: #B5B5C3 !important;
-                  }
-
-                  .font-size-h6 {
-                    font-size: 1.75rem !important;
-                  }
-
                   .text-dark {
                     color: #181C32 !important;
                   }
-
-                  .todo {
-                    color: #3F4254 !important;
-                    font-weight: 500 !important;
-                    margin-bottom: 0.25rem !important;
-                  }
-
-                  .bullet.bullet-bar {
-                    width: 4px;
-                    height: auto;
-                  }
-
-                  .bullet {
-                    display: inline-block;
-                    background-color: #E4E6EF;
-                    width: 10px;
-                    height: 2px;
-                    border-radius: 2rem;
-                    margin-left: 1rem;
-
-                  }
-
-                  .todo-text {
-                    font-size: 1.40rem;
-                  }
-
-                  .bg-success {
-                    background-color: #3699FF !important;
-                  }
-
+           
                   .card-custom {
                     -webkit-box-shadow: 0px 0px 30px 0px rgb(82 63 105 / 5%);
                     box-shadow: 0px 0px 30px 0px rgb(82 63 105 / 5%);
                     border: 0;
                     background: white;
                     border-radius: 0.42rem;
-                  }
-
-                  .delete-icon {
-                    color: #F64E60 !important;
-                    cursor: pointer;
-                    font-size: 2em;
-                  }
-
-                  .edit-icon {
-                    color: #FFA800 !important;
-                    cursor: pointer;
-                    font-size: 2em;
                   }
                 `}</style>
             </div>
